0f67f5cc47fa664bc79853a5e74a4310f7963c16
Tormod Liseth <tormod@sifterstudios.no>
2025-05-31 20:45:49 +0200

    Fix Dependency Injection Conflicts

--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -1,29 +1,3 @@
-import 'package:injectable/injectable.dart';
-import 'package:tava/features/auth/domain/repositories/auth_repository.dart';
-import 'package:tava/features/auth/infrastructure/mock_auth_repository.dart';
-import 'package:tava/features/practice_session/domain/usecases/get_active_session.dart';
-import 'package:tava/features/practice_session/infrastructure/mock_practice_session_repository.dart';
-import 'package:tava/features/progress/domain/usecases/get_practice_stats.dart';
-import 'package:tava/features/progress/infrastructure/mock_progress_repository.dart';
-import 'package:tava/features/settings/domain/repositories/settings_repository.dart';
-import 'package:tava/features/settings/infrastructure/mock_settings_repository.dart';
-
-@module
-abstract class RegisterModule {
-  @dev
-  @LazySingleton(as: AuthRepository)
-  MockAuthRepository get mockAuthRepository => MockAuthRepository();
-  
-  @dev
-  @LazySingleton(as: PracticeSessionRepository)
-  MockPracticeSessionRepository get mockPracticeSessionRepository => 
-      MockPracticeSessionRepository();
-  
-  @dev
-  @LazySingleton(as: ProgressRepository)
-  MockProgressRepository get mockProgressRepository => MockProgressRepository();
-  
-  @dev
-  @LazySingleton(as: SettingsRepository)
-  MockSettingsRepository get mockSettingsRepository => MockSettingsRepository();
-}
+// This file is intentionally left empty.
+// We're removing the RegisterModule class as it's causing duplicate registrations.
+// All repository registrations are now handled directly in their respective files.

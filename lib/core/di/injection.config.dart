d8203b38d43369acae556f6c450bb58805d9260e
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2025-06-02 09:59:05 +0200

    added lots of stuff

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
@@ -8,74 +9,119 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i5;
-import 'package:supabase_flutter/supabase_flutter.dart' as _i6;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
+import 'package:supabase_flutter/supabase_flutter.dart' as _i454;
 
-import '../../features/auth/domain/usecases/check_auth.dart' as _i9;
-import '../../features/auth/domain/usecases/login_user.dart' as _i11;
-import '../../features/auth/domain/usecases/logout_user.dart' as _i12;
-import '../../features/auth/domain/usecases/register_user.dart' as _i13;
-import '../../features/auth/infrastructure/mock_auth_repository.dart' as _i4;
-import '../../features/auth/presentation/bloc/auth_bloc.dart' as _i14;
-import '../../features/dashboard/presentation/bloc/dashboard_bloc.dart' as _i10;
+import '../../features/auth/domain/repositories/auth_repository.dart' as _i787;
+import '../../features/auth/domain/usecases/check_auth.dart' as _i1011;
+import '../../features/auth/domain/usecases/login_user.dart' as _i778;
+import '../../features/auth/domain/usecases/logout_user.dart' as _i419;
+import '../../features/auth/domain/usecases/register_user.dart' as _i198;
+import '../../features/auth/infrastructure/auth_repository_impl.dart' as _i420;
+import '../../features/auth/infrastructure/mock_auth_repository.dart' as _i560;
+import '../../features/auth/presentation/bloc/auth_bloc.dart' as _i797;
+import '../../features/dashboard/presentation/bloc/dashboard_bloc.dart'
+    as _i652;
 import '../../features/exercise_library/presentation/bloc/exercise_library_bloc.dart'
-    as _i3;
-import '../../features/metronome/presentation/bloc/metronome_bloc.dart' as _i7;
+    as _i804;
+import '../../features/metronome/presentation/bloc/metronome_bloc.dart'
+    as _i544;
+import '../../features/practice_session/domain/usecases/get_active_session.dart'
+    as _i879;
+import '../../features/practice_session/domain/usecases/get_recent_sessions.dart'
+    as _i760;
 import '../../features/practice_session/infrastructure/mock_practice_session_repository.dart'
-    as _i8;
+    as _i341;
+import '../../features/practice_session/presentation/bloc/practice_session_bloc.dart'
+    as _i1021;
+import '../../features/progress/domain/usecases/get_practice_stats.dart'
+    as _i1058;
 import '../../features/progress/infrastructure/mock_progress_repository.dart'
-    as _i16;
-import '../../features/settings/domain/usecases/get_settings.dart' as _i18;
-import '../../features/settings/domain/usecases/update_settings.dart' as _i19;
+    as _i251;
+import '../../features/progress/presentation/bloc/progress_bloc.dart' as _i522;
+import '../../features/settings/domain/repositories/settings_repository.dart'
+    as _i674;
+import '../../features/settings/domain/usecases/get_settings.dart' as _i558;
+import '../../features/settings/domain/usecases/update_settings.dart' as _i986;
 import '../../features/settings/infrastructure/mock_settings_repository.dart'
-    as _i17;
-import '../../features/settings/presentation/bloc/settings_bloc.dart' as _i15;
+    as _i473;
+import '../../features/settings/infrastructure/settings_repository_impl.dart'
+    as _i569;
+import '../../features/settings/presentation/bloc/settings_bloc.dart' as _i585;
 
-extension GetItInjectableX on _i1.GetIt {
+const String _prod = 'prod';
+const String _dev = 'dev';
+
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
-    String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
-  }) {
-    final gh = _i2.GetItHelper(
-      this,
-      environment,
-      environmentFilter,
-    );
-    gh.factory<_i3.ExerciseLibraryBloc>(() => _i3.ExerciseLibraryBloc());
-    gh.lazySingleton<_i4.MockAuthRepository>(() => _i4.MockAuthRepository());
-    gh.factory<_i7.MetronomeBloc>(() => _i7.MetronomeBloc());
-    gh.lazySingleton<_i8.MockPracticeSessionRepository>(
-        () => _i8.MockPracticeSessionRepository());
-    gh.factory<_i9.CheckAuth>(
-        () => _i9.CheckAuth(gh<_i4.MockAuthRepository>()));
-    gh.factory<_i10.DashboardBloc>(() => _i10.DashboardBloc());
-    gh.factory<_i11.LoginUser>(
-        () => _i11.LoginUser(gh<_i4.MockAuthRepository>()));
-    gh.factory<_i12.LogoutUser>(
-        () => _i12.LogoutUser(gh<_i4.MockAuthRepository>()));
-    gh.factory<_i13.RegisterUser>(
-        () => _i13.RegisterUser(gh<_i4.MockAuthRepository>()));
-    gh.factory<_i14.AuthBloc>(() => _i14.AuthBloc(
-          checkAuth: gh<_i9.CheckAuth>(),
-          loginUser: gh<_i11.LoginUser>(),
-          logoutUser: gh<_i12.LogoutUser>(),
-          registerUser: gh<_i13.RegisterUser>(),
-        ));
-    gh.factory<_i15.SettingsBloc>(() => _i15.SettingsBloc(
-          getSettings: gh<_i18.GetSettings>(),
-          updateSettings: gh<_i19.UpdateSettings>(),
-        ));
-    gh.lazySingleton<_i16.MockProgressRepository>(
-        () => _i16.MockProgressRepository());
-    gh.lazySingleton<_i17.MockSettingsRepository>(
-        () => _i17.MockSettingsRepository());
-    gh.factory<_i18.GetSettings>(
-        () => _i18.GetSettings(gh<_i17.MockSettingsRepository>()));
-    gh.factory<_i19.UpdateSettings>(
-        () => _i19.UpdateSettings(gh<_i17.MockSettingsRepository>()));
-    return this;
-  }
+_i174.GetIt init(
+  _i174.GetIt getIt, {
+  String? environment,
+  _i526.EnvironmentFilter? environmentFilter,
+}) {
+  final gh = _i526.GetItHelper(
+    getIt,
+    environment,
+    environmentFilter,
+  );
+  gh.factory<_i652.DashboardBloc>(() => _i652.DashboardBloc());
+  gh.factory<_i1021.PracticeSessionBloc>(() => _i1021.PracticeSessionBloc());
+  gh.factory<_i804.ExerciseLibraryBloc>(() => _i804.ExerciseLibraryBloc());
+  gh.factory<_i544.MetronomeBloc>(() => _i544.MetronomeBloc());
+  gh.lazySingleton<_i787.AuthRepository>(
+    () => _i420.AuthRepositoryImpl(gh<_i454.SupabaseClient>()),
+    registerFor: {_prod},
+  );
+  gh.lazySingleton<_i879.PracticeSessionRepository>(
+    () => _i341.MockPracticeSessionRepository(),
+    registerFor: {_dev},
+  );
+  gh.factory<_i419.LogoutUser>(
+      () => _i419.LogoutUser(gh<_i787.AuthRepository>()));
+  gh.factory<_i198.RegisterUser>(
+      () => _i198.RegisterUser(gh<_i787.AuthRepository>()));
+  gh.factory<_i1011.CheckAuth>(
+      () => _i1011.CheckAuth(gh<_i787.AuthRepository>()));
+  gh.factory<_i778.LoginUser>(
+      () => _i778.LoginUser(gh<_i787.AuthRepository>()));
+  gh.lazySingleton<_i1058.ProgressRepository>(
+    () => _i251.MockProgressRepository(),
+    registerFor: {_dev},
+  );
+  gh.lazySingleton<_i674.SettingsRepository>(
+    () => _i473.MockSettingsRepository(),
+    registerFor: {_dev},
+  );
+  gh.lazySingleton<_i787.AuthRepository>(
+    () => _i560.MockAuthRepository(),
+    registerFor: {_dev},
+  );
+  gh.factory<_i558.GetSettings>(
+      () => _i558.GetSettings(gh<_i674.SettingsRepository>()));
+  gh.factory<_i986.UpdateSettings>(
+      () => _i986.UpdateSettings(gh<_i674.SettingsRepository>()));
+  gh.lazySingleton<_i674.SettingsRepository>(
+    () => _i569.SettingsRepositoryImpl(gh<_i460.SharedPreferences>()),
+    registerFor: {_prod},
+  );
+  gh.factory<_i760.GetRecentSessions>(
+      () => _i760.GetRecentSessions(gh<_i879.PracticeSessionRepository>()));
+  gh.factory<_i879.GetActiveSession>(
+      () => _i879.GetActiveSession(gh<_i879.PracticeSessionRepository>()));
+  gh.factory<_i585.SettingsBloc>(() => _i585.SettingsBloc(
+        getSettings: gh<_i558.GetSettings>(),
+        updateSettings: gh<_i986.UpdateSettings>(),
+      ));
+  gh.factory<_i1058.GetPracticeStats>(
+      () => _i1058.GetPracticeStats(gh<_i1058.ProgressRepository>()));
+  gh.factory<_i797.AuthBloc>(() => _i797.AuthBloc(
+        checkAuth: gh<_i1011.CheckAuth>(),
+        loginUser: gh<_i778.LoginUser>(),
+        logoutUser: gh<_i419.LogoutUser>(),
+        registerUser: gh<_i198.RegisterUser>(),
+      ));
+  gh.factory<_i522.ProgressBloc>(() =>
+      _i522.ProgressBloc(getPracticeStats: gh<_i1058.GetPracticeStats>()));
+  return getIt;
 }
